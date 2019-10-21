04b67c801cf64cec5bf90f3d3a07850c6d618b38
Damiano Del Sarto <d.delsarto2@studenti.unipi.it>
2019-10-22 00:40:52 +0200

    Docs theme updated

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 
 import os
 import sys
+import sphinx_rtd_theme
 package_name = 'splrand'
 package_root = os.path.abspath('..')
 sys.path.insert(0, package_root)
@@ -34,7 +35,7 @@ release = '0.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon', "sphinx_rtd_theme", ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,7 +53,8 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
